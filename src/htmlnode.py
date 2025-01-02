8c740dc86a071acf7a422ce911dfe5d364a7920c
kevin4052 <kevin.hernandez001@gmail.com>
2025-01-02 14:41:11 -0600

    LeafNode plus tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,3 +17,14 @@ class HTMLNode:
             for key, value in self.props.items():
                 props += f' {key}="{value}"'
         return props
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("LeafNode value cannot be None")
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHtmlNode(unittest.TestCase):
     def test_repr(self):
@@ -14,3 +14,24 @@ class TestHtmlNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode("div", "This is a div node", None, {"id": "test"})
         self.assertEqual(node.props_to_html(), ' id="test"')
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "This is a paragraph node")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph node</p>")
+
+    def test_to_html_with_props(self):
+        node = LeafNode("a", "This is a link node", {"href": "https://www.boot.dev"})
+        self.assertEqual(node.to_html(), '<a href="https://www.boot.dev">This is a link node</a>')
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(value="This is a paragraph node")
+        self.assertEqual(node.to_html(), "This is a paragraph node")
+
+    def test_to_html_no_value(self):
+        node = LeafNode("p")
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html_no_tag_no_value(self):
+        node = LeafNode()
+        self.assertRaises(ValueError, node.to_html)
